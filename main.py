d276b4e46cd36158e1f860f33997f4ade430cc84
elifcakir <ecakir@usc.edu>
2023-11-28 16:37:44 -0800

    app to main

--- main.py
+++ main.py
@@ -1,16 +1,237 @@
-# This is a sample Python script.
+import re
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import dotenv
+import pandas as pd
+import requests
+import time
+
+import os
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session
+from werkzeug.utils import secure_filename
+from fuzzywuzzy import fuzz
+from dotenv import load_dotenv
+from outlook_email import send_outlook_email
+from title_categorizer import categorize_title
+import logging
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+df = None
+
+app.logger.setLevel(logging.INFO)
+
+app.config['DF_FOLDER'] = 'dataframes'  # Define the key before using it
+
+# Make sure the DataFrame folder exists
+os.makedirs(app.config['DF_FOLDER'], exist_ok=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.route('/send-email', methods=['POST'])
+def send_email_route():
+    # Capture the form data
+    recipient_email = request.form.get('recipient_email')
+    subject = request.form.get('subject')
+    body = request.form.get('body')
+
+    # Call the send_outlook_email function with the form data
+    send_outlook_email(recipient_email, subject, body)
+    return "Email sent successfully!"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Normalize the address
+def normalize_address(address):
+    if pd.isna(address):
+        return ""
+    address = str(address).lower()
+    address = re.sub(r'\b(street)\b', 'st', address)
+    address = re.sub(r'\b(road)\b', 'rd', address)
+    address = re.sub(r'\b(avenue)\b', 'ave', address)
+    # More substitutions as needed
+    address = re.sub(r'[^\w\s]', '', address)
+    address = re.sub(r'\s+', ' ', address).strip()
+    return address
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# without chatgpt
+def are_addresses_similar(addr1, addr2):
+    norm_addr1 = normalize_address(addr1)
+    norm_addr2 = normalize_address(addr2)
+
+    similarity_score = fuzz.ratio(norm_addr1, norm_addr2)
+    return similarity_score > 90
+
+
+def check_owner_occupancy(property_address, mailing_address):
+    norm_property_address = normalize_address(property_address)
+    norm_mailing_address = normalize_address(mailing_address)
+    return norm_property_address == norm_mailing_address
+
+
+@app.route('/process-contact-title', methods=['POST'])
+def process_contact_title():
+    selected_title = request.form['contact_title']
+
+    return redirect(url_for('greeting'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', filename=None)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        try:
+            # Read the uploaded file into a DataFrame
+            df = pd.read_csv(file_path)
+
+            # Check if the 'contact_title' column is in the DataFrame
+            if 'contact_title' not in df.columns:
+                return 'The uploaded file does not contain a "contact_title" column.'
+
+            # Apply the categorization function to the contact_title column
+            df['contact_category'] = df['contact_title'].apply(categorize_title)
+
+            # Create a list of unique categories for the dropdown menu
+            categories = sorted(df['contact_category'].astype(str).unique())
+
+            # Save the DataFrame to a temporary file
+            timestamp = int(time.time())
+            temp_filename = f"df_{timestamp}.pkl"
+            df.to_pickle(os.path.join(app.config['DF_FOLDER'], temp_filename))
+
+            # Pass the categories, temp_filename, and filename back to the index template
+            # The filename is added to the context so that it can be displayed after the page reloads
+            return render_template('index.html', categories=categories, df_file=temp_filename, filename=filename)
+
+        except Exception as e:
+            return jsonify({"error": str(e)})
+    else:
+        return 'Invalid file type'
+
+
+@app.route('/filter_by_category', methods=['POST'])
+def filter_by_category():
+    df_path = request.form['df_path']
+    selected_category = request.form['category']
+    owner_occupier_selection = request.form.get('owner_occupier', 'all')
+
+    full_df_path = os.path.join(app.config['DF_FOLDER'], df_path)
+
+    if not os.path.isfile(full_df_path):
+        return jsonify({"error": "The provided path is not a file."})
+
+        # Load the DataFrame from the temporary file
+    df = pd.read_pickle(full_df_path)
+
+    # Replace 'nan' strings and NaN values with an empty string in the DataFrame
+    df = df.fillna('').replace('nan', '')
+
+    # If "VIEW ALL" is selected, assign the entire DataFrame to filtered_df
+    if selected_category == 'VIEW ALL':
+        filtered_df = df
+        title = 'List of All Contacts'
+    else:
+        # Filter by the selected category
+        filtered_df = df[df['contact_category'] == selected_category]
+
+        # If the category is "Owner" or "Owner/Principal", further filter based on owner occupancy
+        if selected_category in ['Owner', 'Owner/Principal']:
+            if owner_occupier_selection == 'yes':
+                filtered_df = filtered_df[filtered_df.apply(
+                    lambda row: check_owner_occupancy(row['address_full'], row['reported_mailing_address_full']),
+                    axis=1
+                )]
+                title = 'List of Owner-Occupiers'
+            elif owner_occupier_selection == 'no':
+                filtered_df = filtered_df[filtered_df.apply(
+                    lambda row: not check_owner_occupancy(row['address_full'], row['reported_mailing_address_full']),
+                    axis=1
+                )]
+                title = 'List of Owner Non-Occupiers'
+            else:
+                # If "all" is selected, no additional filtering is applied
+                title = 'List of Owners'
+        else:
+            # For other categories, create a title based on the category name
+            title = f'List of {selected_category}s' if not selected_category.endswith(
+                's') else f'List of {selected_category}'
+
+    # Convert the filtered DataFrame to a dictionary for the template
+    properties = filtered_df.to_dict(orient='records')
+
+    # Render the template with the filtered properties and the title
+    return render_template('contact_table.html', properties=properties, title=title)
+
+
+@app.route('/check-occupier', methods=['POST'])
+def check_occupier():
+    owner_occupier = request.form.get('owner_occupier') == 'yes'
+    global df
+    if df is not None and owner_occupier:
+        owner_occupiers = []
+        for _, row in df.iterrows():
+            if are_addresses_similar(row['address_full'], row['reported_mailing_address_full']):
+                owner_occupiers.append(row.to_dict())
+
+        # Render the template with owner-occupiers
+        # print("Owner Occupiers: ", owner_occupiers)
+        return render_template('contact_table.html', properties=owner_occupiers)
+    else:
+        # Handle other cases or return a message
+        return "No owner-occupier information available or DataFrame not loaded."
+
+
+@app.route('/list-owner-principals')
+def list_owner_principals():
+    global df
+    if df is not None:
+        owner_principals_df = df[df['contact_title'] == 'Owner/Principal']
+        properties = owner_principals_df.to_dict(orient='records')
+        return render_template('contact_table.html', properties=properties)
+    else:
+        return "DataFrame is not loaded or file not uploaded."
+
+
+@app.route('/create-email', methods=['POST'])
+def create_email():
+    # Retrieve address and full name from form data
+    full_address = request.form.get('address')
+    # Split the full address by comma and take the first part (street address)
+    street_address = full_address.split(',')[0] if full_address else 'Address not provided'
+    recipient_email = request.form.get('contact_email')
+    full_name = request.form.get('full_name')
+    first_name = full_name.split()[0] if full_name else 'Valued Customer'
+    subject = f"Potential Offer for {street_address}"
+    body = f"""
+    {first_name}- 
+
+    I am reaching out to you about your building at {street_address}. Based on my research, it looks like you own and operate your facility. Would you be willing to look at an unsolicited offer to purchase your building, either with you leaving, or by monetizing the asset now and leasing it back?
+
+    If so, can I give you a 5-minute call just to see what is important to you so I can present an offer? Thanks in advance.
+    """
+
+    return render_template('email_template.html', recipient_email=recipient_email, body=body, subject=subject)
+
+
+@app.route('/email')
+def email_page():
+    return "Email Page"
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
+
+
